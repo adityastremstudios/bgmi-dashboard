d12068eee146beb749a5926949a3a06c0551101c
adityastremstudios <aslovesh@gmail.com>
2025-09-16 13:24:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,6 +61,5 @@ app.post("/end-match", async (req, res) => {
   }
 });
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
